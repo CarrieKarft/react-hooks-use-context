056f882660b9b0881290d019ce2ce5c56f9b5691
Carrie Kraft <carriekarft@gmail.com>
2023-02-15 11:27:03 -0800

    commit tree 7f776b5ac63e96c744a696d95b3050e88d764ea6

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, {useContext} from "react";
+import { UserContext } from "../context/user";
+import { ThemeContext } from "../context/theme";
 import Interests from "./Interests";
 
-function Profile({ user, theme }) {
+function Profile() {
+  const {theme} = useContext(ThemeContext)
+  const { user } = useContext(UserContext);
   if (!user) return <h2>Please Login To View Profile</h2>;
   return (
     <div>
